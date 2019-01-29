f4af84279b24c25b1d9704d9b4b12063c209b5f6
pankajchouhan <pankajchouhan80@gmail.com>
2019-01-29 21:39:51 +0530

    Initial commit

---
+++ app/src/main/java/in/p4n/ratingbar/MainActivity.java
@@ -0,0 +1,34 @@
+package in.p4n.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingbar;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+    public void addListenerOnButtonClick(){
+        ratingbar=(RatingBar)findViewById(R.id.ratingBar);
+        button=(Button)findViewById(R.id.button);
+        //Performing action on Button Click
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                //Getting the rating and displaying it on the toast
+                String rating=String.valueOf(ratingbar.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
